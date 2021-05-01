2a1a8c75ff5232acf5dd7ef2a7ce8dacc43b64e5
ononak <ononak@gmail.com>
2021-05-01 18:35:03 +0300

    -
    -

--- Test/demo_filter.m
+++ Test/demo_filter.m
@@ -7,8 +7,8 @@ x = sin(t);
 n = rand(1,length(t));
 x = x+n;
     
-i= 10;
-x = ts.potvals(i,1:1000);
+i= 2;
+x = ts.potvals(i,1:3000);
 
 ws = 15;
 fsignal = Movav(x,ws);
@@ -19,7 +19,7 @@ title(['moving average filter' ' window size = ' num2str(ws)])
 legend({'noisy' 'filtered'})
 
 %% High pass filter for base line wander drift
-i =60;
+i =2;
 filterType = 'butterworth';
 [fsignal] = Bwdfilter(ts.potvals(i,:),2048,filterType);
 figure(2);
@@ -32,7 +32,7 @@ legend({'noisy' 'filtered'})
 
 fs =2048
 
-i =10;
+i =1;
 filterType = 'butterworth';
 noisy = ts.potvals(i,1:1000);
 fstop = 50;
